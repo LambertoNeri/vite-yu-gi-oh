b8d4db80d57add501019659af38b3726f84c6bba
LambertoNeri <neri.lamberto91@gmail.com>
2023-05-22 15:34:37 +0200

    add store but not working as intended

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,26 @@
   import AppHeader from './components/AppHeader.vue';
   import AppMain from './components/AppMain.vue';
   import AppFooter from './components/AppFooter.vue';
+  import axios from 'axios';
+  import { store } from './store'
 
 
   export default {
+    data() {
+      return {
+        store,
+      };
+    },
     components: {
       AppHeader,
       AppMain,
       AppFooter,
-    }
+    },
+    creted() {
+      axios
+        .get('https://db.ygoprodeck.com/api/v7/cardinfo.php?&fname=Acrobatic Magician')
+        .then(response => (this.store.cardList = response.data[2].name));
+    },
   }
 </script>
 
